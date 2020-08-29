bfb1ec4ea786a15c02e99ca5a65ed59815780165
Aron Arias <aron.ar.96@gmail.com>
2020-08-29 11:49:06 -0500

    component banner finished

--- src/app/home/components/banner/banner.component.ts
+++ src/app/home/components/banner/banner.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
 })
 export class BannerComponent implements OnInit {
+  banner = [
+    {
+      image: './../../../../assets/images/portada.jpg',
+      titulo: 'Solicita tu asesoría gratuita',
+      urlBtn: '/home',
+    },
+    {
+      image: './../../../../assets/images/portada.jpg',
+      titulo: 'Hazte voluntario',
+      urlBtn: '/home',
+    },
+    {
+      image: './../../../../assets/images/portada.jpg',
+      titulo: 'Algunas asesorías brindadas por nuestros voluntarios',
+      urlBtn: '/home',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
