ee2bb4465672165fd11094c593b8079220ae6e14
Aron Arias <aron.ar.96@gmail.com>
2020-08-29 11:18:42 -0500

    creacion del banner y arreglo de bugs responsive en footer

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 
-import { HomeRoutingModule } from './home-routing.module'
+import { HomeRoutingModule } from './home-routing.module';
+import { BannerComponent } from './components/banner/banner.component'
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, BannerComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
